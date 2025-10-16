fce32a722d095eff0ffd92b05584b0f28f27b597
Петрова Кристина Дмитриевна <petrovakd@astra-176>
2025-10-16 17:57:31 +0300

    edit planner.py. Added function create_task and other

--- planner.py
+++ planner.py
@@ -1,6 +1,6 @@
 import json
 import sys_setting
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import os
 
 class Planner:
@@ -13,16 +13,33 @@ class Planner:
         self.is_done = is_done
         self.deadline = deadline
         
-#     def add_task(self):
-        
+    def add_task(self):
+        pass
+def creat_addedTask():
+    pass           
+ 
 def create_task():
     os.system('clear')
     text_task = input("Введите задачу: ")
     print("Нажмите 'd', если нужно добавить подзадачу\nНажмите 'f' если нужно добавить дедлайн")
+    add_to_task = None
+    deadline = None
     key = sys_setting.get_key()
-    # if key == 'd':
+    if key == 'd':
+        add_to_task = creat_addedTask()
+    if key == 'f':
+        deadline = input("Введите дату дедлайна (в формате: гггг-мм-дд): ")
+    
+    task1 = Planner(id=None, 
+                    date = date.today(), 
+                    time_date = datetime.now(),
+                    task = text_task,
+                    is_done=False,
+                    add_to_task=add_to_task if add_to_task else "none",
+                    deadline=deadline if deadline else "none")
+    task1.add_task()
         
-
+   
 def print_date(today_date):
     if today_date == date.today():
         print(f"____________{today_date} (сегодня)____________")
