09a3e228f1d1f65c3be997b0b854a0f72cddc214
Петрова Кристина Дмитриевна <petrovakd@astra-176>
2025-10-20 15:35:11 +0300

    add task

--- lab_tasks/task2.3.py
+++ lab_tasks/task2.3.py
@@ -1,2 +1,11 @@
-# def is_prime(number):
+def is_prime(number):
     
+    for i in range(2, number):
+        if number % i == 0:
+            print("Число не является простым")
+            break
+    else:
+        print("Число простое")
+            
+a = int(input("Введите число: "))
+is_prime(a)
